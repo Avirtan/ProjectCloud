54b94832151683465cfb2c6220c1727c70461cc7
Avirtan <36964697+Avirtan@users.noreply.github.com>
2019-09-18 17:04:20 +0300

    еуце

--- ProjectCloud/Main.Designer.cs
+++ ProjectCloud/Main.Designer.cs
@@ -32,7 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Локальные", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Глобальные", System.Windows.Forms.HorizontalAlignment.Left);
-            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.FileView = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
@@ -40,24 +39,10 @@
             this.RefreshFile = new MaterialSkin.Controls.MaterialFlatButton();
             this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
             this.materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
-            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.AddFile = new MaterialSkin.Controls.MaterialFlatButton();
+            this.DeleteFile = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton6 = new MaterialSkin.Controls.MaterialFlatButton();
             this.SuspendLayout();
-            // 
-            // materialFlatButton1
-            // 
-            this.materialFlatButton1.AutoSize = true;
-            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialFlatButton1.Depth = 0;
-            this.materialFlatButton1.Location = new System.Drawing.Point(12, 319);
-            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialFlatButton1.Name = "materialFlatButton1";
-            this.materialFlatButton1.Primary = false;
-            this.materialFlatButton1.Size = new System.Drawing.Size(172, 36);
-            this.materialFlatButton1.TabIndex = 0;
-            this.materialFlatButton1.Text = "materialFlatButton1";
-            this.materialFlatButton1.UseVisualStyleBackColor = true;
-            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
             // 
             // imageList1
             // 
@@ -69,9 +54,9 @@
             // FileView
             // 
             listViewGroup1.Header = "Локальные";
-            listViewGroup1.Name = "listg";
+            listViewGroup1.Name = "local";
             listViewGroup2.Header = "Глобальные";
-            listViewGroup2.Name = "listViewGroup1";
+            listViewGroup2.Name = "global";
             this.FileView.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
             listViewGroup1,
             listViewGroup2});
@@ -83,7 +68,6 @@
             this.FileView.SmallImageList = this.imageList1;
             this.FileView.TabIndex = 1;
             this.FileView.UseCompatibleStateImageBehavior = false;
-            this.FileView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // button1
             // 
@@ -152,20 +136,52 @@
             this.materialFlatButton4.Text = "Скачать файл";
             this.materialFlatButton4.UseVisualStyleBackColor = true;
             // 
-            // materialFlatButton2
+            // AddFile
             // 
-            this.materialFlatButton2.AutoSize = true;
-            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialFlatButton2.Depth = 0;
-            this.materialFlatButton2.Location = new System.Drawing.Point(13, 204);
-            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialFlatButton2.Name = "materialFlatButton2";
-            this.materialFlatButton2.Primary = false;
-            this.materialFlatButton2.Size = new System.Drawing.Size(128, 36);
-            this.materialFlatButton2.TabIndex = 7;
-            this.materialFlatButton2.Text = "Добавить файл";
-            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.AddFile.AutoSize = true;
+            this.AddFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AddFile.Depth = 0;
+            this.AddFile.Location = new System.Drawing.Point(13, 204);
+            this.AddFile.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.AddFile.MouseState = MaterialSkin.MouseState.HOVER;
+            this.AddFile.Name = "AddFile";
+            this.AddFile.Primary = false;
+            this.AddFile.Size = new System.Drawing.Size(128, 36);
+            this.AddFile.TabIndex = 7;
+            this.AddFile.Text = "Добавить файл";
+            this.AddFile.UseVisualStyleBackColor = true;
+            this.AddFile.Click += new System.EventHandler(this.AddFile_Click);
+            // 
+            // DeleteFile
+            // 
+            this.DeleteFile.AutoSize = true;
+            this.DeleteFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.DeleteFile.Depth = 0;
+            this.DeleteFile.Location = new System.Drawing.Point(13, 250);
+            this.DeleteFile.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.DeleteFile.MouseState = MaterialSkin.MouseState.HOVER;
+            this.DeleteFile.Name = "DeleteFile";
+            this.DeleteFile.Primary = false;
+            this.DeleteFile.Size = new System.Drawing.Size(119, 36);
+            this.DeleteFile.TabIndex = 8;
+            this.DeleteFile.Text = "Удалить файл";
+            this.DeleteFile.UseVisualStyleBackColor = true;
+            this.DeleteFile.Click += new System.EventHandler(this.DeleteFile_Click);
+            // 
+            // materialFlatButton6
+            // 
+            this.materialFlatButton6.AutoSize = true;
+            this.materialFlatButton6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton6.Depth = 0;
+            this.materialFlatButton6.Location = new System.Drawing.Point(13, 296);
+            this.materialFlatButton6.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton6.Name = "materialFlatButton6";
+            this.materialFlatButton6.Primary = false;
+            this.materialFlatButton6.Size = new System.Drawing.Size(223, 36);
+            this.materialFlatButton6.TabIndex = 9;
+            this.materialFlatButton6.Text = "Посмотреть пользователей";
+            this.materialFlatButton6.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
@@ -173,14 +189,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
             this.ClientSize = new System.Drawing.Size(809, 464);
-            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.materialFlatButton6);
+            this.Controls.Add(this.DeleteFile);
+            this.Controls.Add(this.AddFile);
             this.Controls.Add(this.materialFlatButton4);
             this.Controls.Add(this.materialFlatButton3);
             this.Controls.Add(this.RefreshFile);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.FileView);
-            this.Controls.Add(this.materialFlatButton1);
             this.MaximumSize = new System.Drawing.Size(809, 464);
             this.MinimumSize = new System.Drawing.Size(809, 464);
             this.Name = "Main";
@@ -192,8 +209,6 @@
         }
 
         #endregion
-
-        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.ListView FileView;
         private System.Windows.Forms.Button button1;
@@ -201,6 +216,8 @@
         private MaterialSkin.Controls.MaterialFlatButton RefreshFile;
         private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
         private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
-        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialFlatButton AddFile;
+        private MaterialSkin.Controls.MaterialFlatButton DeleteFile;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton6;
     }
 }
--- ProjectCloud/Main.cs
+++ ProjectCloud/Main.cs
@@ -23,21 +23,19 @@ namespace ProjectCloud
             InitializeComponent();
         }
 
-        private string pathArchiv = @"archiv\e.zip";
+       // private string pathArchiv = @"archiv\e.zip";
         private string ftpLogin = "z95230kj_root";
         private string ftpPass = "VOcxjN%0";
         private string ftpUrl = @"ftp://z95230kj.beget.tech/Cloud/";
         private string pathLocal = @"Temp\";
-        private ArrayList listFileFtp = new ArrayList();
-
-
+       //private string pass = "12345";
+        
         private void Main_Load(object sender, EventArgs e)
         {
             var skin = MaterialSkinManager.Instance;
             skin.AddFormToManage(this);
             skin.Theme = MaterialSkinManager.Themes.DARK;
             skin.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
-
             string[] files = Directory.GetFiles(pathLocal);
             foreach (string file in files)
             {
@@ -48,39 +46,13 @@ namespace ProjectCloud
                 FileView.Items.Add(lvi);
             }
         }
-
-        private void materialFlatButton1_Click(object sender, EventArgs e)
-        {
-            ZipFile zip = new ZipFile();
-            zip.AddDirectory(@"archiv");
-            zip.Save(pathArchiv);
-            //ZipFile zip2 = new ZipFile();
-            //zip2.Password = "123";
-            //zip2.AddDirectory(@"C:\Users\svini\OneDrive\Desktop\проект\rar");
-            //zip2.Save(@"C:\Users\svini\OneDrive\Desktop\проект\e.zip");
-            //using (ZipFile z =new  ZipFile(@"C:\Users\svini\OneDrive\Desktop\проект\e.zip"))
-            //{
-            //    z.ExtractAll(@"C:\Users\svini\OneDrive\Desktop\проект\rar");
-            //}
-            
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ListView lst = (ListView)sender;
-            //MessageBox.Show(listView1.SelectedItems.);
-        }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            using (ZipFile zp = ZipFile.Read(pathArchiv))
-            {
-                foreach (ZipEntry z in zp)
-                {
-                    //z.Extract("to/");
-                    MessageBox.Show(z.FileName);
-                }
-            }
+           ZipFile zip2 = new ZipFile();
+           zip2.AddFile("1.txt");
+           zip2.Save("Cloud.zip");
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -103,8 +75,6 @@ namespace ProjectCloud
             //request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
 
             
-            
-
             //WebClient client = new WebClient();
             //client.Credentials = new NetworkCredential(ftpLogin,ftpPass);
             //client.UploadFile("ftp://ftp.example.com/remote/path/file.zip", @"C:\local\path\file.zip");
@@ -113,24 +83,71 @@ namespace ProjectCloud
 
         private void RefreshFile_Click(object sender, EventArgs e)
         {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
-            request.Method = WebRequestMethods.Ftp.ListDirectory;
-            request.Credentials = new NetworkCredential(ftpLogin, ftpPass);
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream);
-            string line;
-            while ((line = reader.ReadLine()) != null) listFileFtp.Add(line);
-            reader.Close();
-            response.Close();
-            foreach (object o in listFileFtp)
+            FileView.Items.Clear();
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)
             {
-                ListViewItem lvi = new ListViewItem();
-                lvi.Text = o.ToString();
-                lvi.ImageIndex = 0;
-                lvi.Group = FileView.Groups[1];
-                FileView.Items.Add(lvi);
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                request.Credentials = new NetworkCredential(ftpLogin, ftpPass);
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                string line;
+                ArrayList listFileFtp = new ArrayList();
+                while ((line = reader.ReadLine()) != null) listFileFtp.Add(line);
+                reader.Close();
+                response.Close();
+                foreach (object o in listFileFtp)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = o.ToString();
+                    lvi.ImageIndex = 0;
+                    lvi.Group = FileView.Groups[1];
+                    FileView.Items.Add(lvi);
+                }
             }
+            using (ZipFile zip = ZipFile.Read("Cloud.zip"))
+            {
+                foreach (ZipEntry z in zip)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = z.FileName;
+                    lvi.ImageIndex = 0;
+                    lvi.Group = FileView.Groups[0];
+                    FileView.Items.Add(lvi);
+                }
+            }
+        }
+
+        private void DeleteFile_Click(object sender, EventArgs e)
+        {
+            if (FileView.FocusedItem.Group.ToString() == "Глобальные")
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl+ FileView.FocusedItem.Text);
+                request.Credentials = new NetworkCredential(ftpLogin, ftpPass);
+                request.Method = WebRequestMethods.Ftp.DeleteFile;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                response.Close();
+                Button btn = new Button();
+                btn.Click += RefreshFile_Click;
+                btn.PerformClick();
+            }
+            if (FileView.FocusedItem.Group.ToString() == "Локальные")
+            {
+                
+            }
+        }
+
+        private void AddFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) {
+                string filename = openFileDialog1.FileName;
+            }
+            
         }
     }
 }
